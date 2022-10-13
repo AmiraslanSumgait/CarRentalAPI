0591730de2f61e0f4ec6a4b06b54a877fb97f4d6
AmiraslanSumgait <emiraslaneliyev45@gmail.com>
2022-10-13 14:01:25 +0400

    initial commit

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,57 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>()
+            {
+                 new Car{ CarId=1,BrandId=1, ColorId=1, DailyPrice=200, Description="Bmw e39", ModelYear=2002},
+                 new Car{ CarId=2,BrandId=1, ColorId=1, DailyPrice=300, Description="Bmw e440", ModelYear=2010},
+                 new Car{ CarId=3,BrandId=2, ColorId=1, DailyPrice=400, Description="Bmw e50", ModelYear=2012},
+                 new Car{ CarId=4,BrandId=2, ColorId=1, DailyPrice=500, Description="Bmw e593", ModelYear=2022}
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+           _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetAllByBrand(int brandId)
+        {
+           return _cars.Where(c=>c.BrandId==brandId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            carToUpdate.CarId=car.CarId;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+    }
+}
